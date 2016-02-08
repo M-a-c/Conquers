72e0935594a4878800ca6e6ad7d31b353903bc7f
Joe Kallarackel <jkalla2@uic.edu>
2016-02-08 12:18:41 -0600

    Main Menu Test
    Ghetto Rig main menu

--- CS440/CS440/main.cpp
+++ CS440/CS440/main.cpp
@@ -1,24 +1,65 @@
 #include <SFML/Graphics.hpp>
+#include "Menu.h"
+#include <iostream>
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode(1200, 600), "My Ghetto Rig Menu!");
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+  Menu menu(window.getSize().x, window.getSize().y);
 
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
 
-	return 0;
+  while (window.isOpen())
+  {
+    sf::Event event;
+
+    while (window.pollEvent(event))
+    {
+
+      switch (event.type)
+      {
+        case sf::Event::Closed:
+          window.close();break;
+        
+        case sf::Event::KeyReleased:
+          switch (event.key.code)
+          {
+            case sf::Keyboard::Up:
+              menu.W_ButtonPress(); break;
+            case sf::Keyboard::Down:
+              menu.S_ButtonPress(); break;
+            case sf::Keyboard::Return:
+              if (menu.getEnterPress() == 0)
+              {
+                std::cout << "Login\n";
+              }
+              else if (menu.getEnterPress() == 1)
+              {
+                std::cout << "New\n";
+              }
+              else if (menu.getEnterPress() == 2)
+              {
+                std::cout << "Help\n";
+              }
+              else if (menu.getEnterPress() == 3)
+              {
+                std::cout << "Exit\n";
+                window.close();
+              }
+                
+          }
+
+      }
+
+    }
+
+    window.clear();
+
+    menu.draw(window);
+
+    window.display();
+  }
+
+  return 0;
 }
